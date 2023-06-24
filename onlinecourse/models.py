b8e990daf6cf07fd1f060e24b70ac91f0dd35139
mahmoudsamneh <mahmoud.samneh@yahoo.com>
2023-06-24 15:37:23 +0300

    adding pipfiles, virtual env

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,6 +105,28 @@ class Enrollment(models.Model):
     # Foreign key to lesson
     # question text
     # question grade/mark
+    
+class  Question(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    question_text = models.TextField()
+    grade = models.IntegerField(default = 0)
+    
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+        
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete= models.CASCADE)
+    choice_text = models.TextField()
+    is_correct = models.BooleanField(default = False)
+    
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     #def is_get_score(self, selected_ids):
